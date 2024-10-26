24d71009d447c9dd681a96aa069f0e03e9174881
Jean-Louis Voiseux <48380853+jlvoiseux@users.noreply.github.com>
2024-10-26 18:38:17 +0300

    CI fixes

--- test/config.py
+++ test/config.py
@@ -2,4 +2,4 @@ import os
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(TEST_DIR)
-BRITTEN_PATH = os.path.join(PROJECT_ROOT, "target/x86_64-unknown-linux-gnu/release/britten")
+BRITTEN_PATH = os.getenv('BRITTEN_PATH', os.path.join(os.path.dirname(os.path.dirname(__file__)), "target/x86_64-unknown-linux-gnu/release/britten"))
